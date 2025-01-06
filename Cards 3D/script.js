91070a13882efa428bf48bc9f743f3ebc745061f
PedroVViana <86854663+PedroVViana@users.noreply.github.com>
2025-01-06 10:30:45 -0300

    Add files via upload

---
+++ Cards 3D/script.js
@@ -0,0 +1,15 @@
+VanillaTilt.init(document.querySelectorAll(".js-tilt"), {
+    max: 25,
+    speed: 400
+});
+
+const sizes = document.querySelectorAll(".size");
+
+for (let i = 0; 1< sizes.length; i++) {
+    sizes[i].addEventListener("click", () => {
+        for (let i = 0; i < sizes.length; i++) {
+            sizes[i].classList.remove("active");
+        }
+        sizes[i].classList.add("active");
+    });
+}
